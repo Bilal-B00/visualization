c98261918b4f42a1c39d022149acee4698badc49
TychonBos <54850632+TychonBos@users.noreply.github.com>
2022-01-19 20:50:38 +0100

    Add files via upload

--- dashframework-main/app.py
+++ dashframework-main/app.py
@@ -1,20 +1,23 @@
 from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
-from jbi100_app.views.scatterplot import Scatterplot
+from jbi100_app.views.line_graph import linegraph
 
 from dash import html
 import plotly.express as px
 from dash.dependencies import Input, Output
+import pandas as pd
+from dash import dcc
 
 
 if __name__ == '__main__':
     # Create data
-    df = px.data.iris()
+    df = pd.read_csv('C:/Users/Tychon Bos/Google Drive/CSAI/Visualization/data.csv')
 
     # Instantiate custom views
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
-
+    #scatterplot1 = Scatterplot("Scatterplot 1", 'male', 'accident_count', df)
+    #scatterplot2 = Scatterplot("Scatterplot 2", 'male', 'accident_count', df)
+    line = linegraph('accident_index', 'accident_year_x', [1,2,3,4], 'sex_of_driver' ,df)
+   
     app.layout = html.Div(
         id="app-container",
         children=[
@@ -29,30 +32,23 @@ if __name__ == '__main__':
             html.Div(
                 id="right-column",
                 className="nine columns",
-                children=[
-                    scatterplot1,
-                    scatterplot2
+                children=[dcc.Graph(figure = line, id='grpah')
                 ],
             ),
         ],
     )
-
-    # Define interactions
     @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
-
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
-
+        Output(component_id = 'grpah', component_property = 'figure' ),
+        Input(component_id = 'LineGraph' , component_property = 'value' ))
+    
+    def update(value):
+        if value == 'Male':
+            return linegraph('accident_index', 'accident_year_x', [1], 'sex_of_driver' ,df)
+        elif value == 'Female':
+            return linegraph('accident_index', 'accident_year_x', [2], 'sex_of_driver' ,df)
+        elif value == 'Other':
+            return linegraph('accident_index', 'accident_year_x', [3], 'sex_of_driver' ,df)
+        elif value == 'Total':
+            return linegraph('accident_index', 'accident_year_x', [4], 'sex_of_driver' ,df)
 
     app.run_server(debug=False, dev_tools_ui=False)
