f3fef93c22dbc303e3e9476551f4caced834d379
Bilal-B00 <70588680+Bilal-B00@users.noreply.github.com>
2022-02-02 12:06:49 +0100

    Add files via upload

---
+++ dashframework-main/jbi100_app/views/histogram.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 12:31:05 2022
+
+@author: Bilal
+"""
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+def histogram(col_name, select_col, select_val, x_name, df):
+    if select_col == '':
+        uniq_x = np.sort(df[(df[col_name] > 0) & (df[col_name] < 24)][col_name].unique())
+        freq = []
+        for i in uniq_x:
+            freq.append(df[df[col_name] == i].shape[0])
+        fig = px.bar(x = uniq_x, y = freq)
+        fig.update_layout(bargap = 0.2)
+        fig.update_xaxes(title_text = "Age Band")
+        fig.update_yaxes(title_text = "Number of Accident")
+        return fig
+    else:
+        uniq_x = np.sort(df[(df[col_name] > 0) & (df[col_name] < 24)][col_name].unique())
+        freq = []
+        for i in uniq_x:
+            freq.append(df[(df[col_name] == i) & (df[select_col] == select_val)].shape[0])
+        fig = px.bar(x = uniq_x, y = freq)
+        fig.update_layout(bargap = 0.2)
+        fig.update_xaxes(title_text = x_name)
+        fig.update_yaxes(title_text = "Frequency")
+        return fig
---
+++ dashframework-main/jbi100_app/views/population_pyramid.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from dash import dcc, html
+
+def populationPyramid(sex_col, age_band_col, df, year):
+    fig = make_subplots( rows = 1, cols = 2, horizontal_spacing = 0 )
+            
+    age_band = np.sort( df[ age_band_col ].unique() )
+            
+    men_bin = []
+    women_bin = []
+            
+    for i in age_band:
+        if i == -1:
+            continue
+        else:
+            men_bin.append( df[ ( df[ sex_col ] == 1 ) & ( df[ age_band_col ]  == i ) & ( df[ 'accident_year_x' ] == year ) ].shape[0] )
+            women_bin.append( df[ ( df[ sex_col ] == 2 ) & ( df[ age_band_col ]  == i ) & ( df[ 'accident_year_x' ] == year ) ].shape[0] )
+                    
+    men_bin = np.array( men_bin )
+    women_bin = np.array( women_bin )
+            
+    bar_one = go.Bar( name = 'Male', x = men_bin, y = age_band[1:], orientation = 'h', text = men_bin, hoverinfo = 'text' )
+    bar_two = go.Bar( name = 'Female', x = women_bin, y = age_band[1:], orientation = 'h', text = women_bin, hoverinfo = 'text' )
+            
+    fig.add_trace( bar_one, row = 1, col = 1 )
+    fig.add_trace( bar_two, row = 1, col = 2 )
+    
+    maximum = np.concatenate((women_bin, men_bin)).max()
+            
+    fig.update_xaxes( autorange = 'reversed', row = 1, col = 1, range = [0, 200000] )
+    fig.update_xaxes( autorange = False, row = 1, col = 2, range = [0, maximum] )
+    fig.update_xaxes( title_text = 'Number of Accidents', row = 1, col = 1 )
+    fig.update_yaxes( title_text = 'Age Band of Driver', row = 1, col = 1 )
+    fig.update_yaxes( showticklabels = False, row = 1, col = 2 )
+            
+    return fig
