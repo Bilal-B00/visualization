52437fe4007fcb0b79b91763edc48a42707f808e
Bilal-B00 <70588680+Bilal-B00@users.noreply.github.com>
2022-02-03 19:11:25 +0100

    Add files via upload

--- dashframework-main/jbi100_app/views/histogram.py
+++ dashframework-main/jbi100_app/views/histogram.py
@@ -9,7 +9,9 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+# initialize histogram function
 def histogram(col_name, select_col, select_val, x_name, df):
+    # calculate histogram without ant=y filtering criteria 
     if select_col == '':
         uniq_x = np.sort(df[(df[col_name] > 0) & (df[col_name] < 24)][col_name].unique())
         freq = []
@@ -18,8 +20,9 @@ def histogram(col_name, select_col, select_val, x_name, df):
         fig = px.bar(x = uniq_x, y = freq)
         fig.update_layout(bargap = 0.2)
         fig.update_xaxes(title_text = "Age Band")
-        fig.update_yaxes(title_text = "Number of Accident")
+        fig.update_yaxes(title_text = "Number of Accidents")
         return fig
+    # calculate histogram with filtering criteria
     else:
         uniq_x = np.sort(df[(df[col_name] > 0) & (df[col_name] < 24)][col_name].unique())
         freq = []
@@ -28,5 +31,5 @@ def histogram(col_name, select_col, select_val, x_name, df):
         fig = px.bar(x = uniq_x, y = freq)
         fig.update_layout(bargap = 0.2)
         fig.update_xaxes(title_text = x_name)
-        fig.update_yaxes(title_text = "Frequency")
+        fig.update_yaxes(title_text = "Number of Accidents")
         return fig
